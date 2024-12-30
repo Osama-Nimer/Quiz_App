a48a6efed6cda6e24fd2aa3c9dd7e9de6226736d
Osama-Nimer <osamafares23@yahoo.com>
2024-12-30 21:53:29 +0300

    Quizzes Done!

--- Quiz_Api/Data_Layer/obj/Debug/net8.0/Data_Layer.AssemblyInfo.cs
+++ Quiz_Api/Data_Layer/obj/Debug/net8.0/Data_Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data_Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c3bb1477365336e46db2e1e480c365f2eb9adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82881ca4023647ca067af1822f7b805bb3067166")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data_Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data_Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
